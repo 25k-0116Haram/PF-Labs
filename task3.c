6a07c85b0e347644ae834539c9004ab4ec232ff8
25k-0116Haram <K250116@nu.edu.pk>
2025-09-17 23:45:46 +0500

    updated commit

---
+++ task3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	int num;
+	
+	printf("Enter an integer(between 65 to 90):");
+	scanf("%d", &num);
+	
+	if (num>65 && num<90){
+		printf("%d= %c\n",num,num);
+	}else {
+		printf("Invalid input! Please enter a number between 65 to 90.\n");
+	}
+	return 0;
+}
